b35b9ac53b68d3a8c18cd83ae217d51adbb84839
Kluivert <39888432+Tristankluivert@users.noreply.github.com>
2019-01-02 10:01:25 +0100

    Added little features

--- app/src/main/java/com/ostap_kozak/newsfeed/FeedActivity.java
+++ app/src/main/java/com/ostap_kozak/newsfeed/FeedActivity.java
@@ -1,10 +1,12 @@
 package com.ostap_kozak.newsfeed;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -20,16 +22,25 @@ import com.android.volley.toolbox.Volley;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FeedActivity extends AppCompatActivity {
+public class FeedActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     private List<News> news = new ArrayList<>();
 
+    SwipeRefreshLayout swipeLayout;
     private final String URL = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=5fdf8e4518634a28be9c18dde43e5c59";
+ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+    progressDialog = new ProgressDialog(this);
+        swipeLayout.setOnRefreshListener(this);
 
+        progressDialog.setTitle("Please wait....");
+        progressDialog.setMessage("Just a little while please");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         // Request a string response from the provided URL.
@@ -37,18 +48,21 @@ public class FeedActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        progressDialog.dismiss();
                         news = Utils.extractNewsFromJson(response);
                         createAdapter();
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(FeedActivity.this, "Connection failed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(FeedActivity.this, "Oops! Connection failed", Toast.LENGTH_SHORT).show();
             }
         });
 
         requestQueue.add(stringRequest);
     }
+
+
 
 
     public void createAdapter() {
@@ -74,5 +88,18 @@ public class FeedActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onRefresh() {
 
+        new Handler().postDelayed(new Runnable() {
+
+            @Override public void run() {
+
+                swipeLayout.setRefreshing(false);
+
+            }
+        }, 5000);
+
+
+    }
 }
